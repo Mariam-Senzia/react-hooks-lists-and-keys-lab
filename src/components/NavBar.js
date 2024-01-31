394f7264b6446510f65e2da38ecbd2ac6002c161
Mariam Senzia <mariamsenzia@gmail.com>
2024-01-31 06:47:32 +0300

    modified files to pass tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const newLinks = links.map((element) => {
+    return <a key={element} href={`#${element}`}>{element}</a>;
+  })
+  return (
+     <nav>
+      {newLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
